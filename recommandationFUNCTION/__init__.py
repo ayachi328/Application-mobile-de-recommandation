d0bb94a120016ad86f3ff047d2b797d4dd26ef8c
ayachi328 <109441962+ayachi328@users.noreply.github.com>
2022-07-22 10:47:30 +0200

    Update __init__.py

--- recommandationFUNCTION/__init__.py
+++ recommandationFUNCTION/__init__.py
@@ -3,14 +3,14 @@ import json
 import azure.functions as func
 
 
-def main(req: func.HttpRequest, todoitems: func.DocumentList) -> func.HttpResponse:
-    if not todoitems:
+def main(req: func.HttpRequest, recommendationDOCUMENT: func.DocumentList) -> func.HttpResponse:
+    if not recommendationDOCUMENT:
         logging.warning("ToDo item not found")
     else:
         logging.info("Found ToDo item, Description=%s",
-                     len(todoitems))
+                     len(recommendationDOCUMENT))
                      
-    result = json.dumps(todoitems[0]['articles'])
+    result = json.dumps(recommendationDOCUMENT[0]['articles'])
     #result = str(result)
     return func.HttpResponse(result, 
     mimetype="application/json", 
